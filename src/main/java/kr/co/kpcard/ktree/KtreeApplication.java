951adb21552a19e40ce0e8ecd73ca7d53cf897e3
MacBook Air M1 <prettyfly@korea.com>
2026-01-29 21:19:02 +0900

    initial commit

---
+++ src/main/java/kr/co/kpcard/ktree/KtreeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kpcard.ktree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtreeApplication.class, args);
+	}
+
+}
